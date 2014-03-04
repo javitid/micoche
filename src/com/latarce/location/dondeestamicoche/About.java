cac1af13a9ea86a4d7850dc7f0db23f798588ee5
Javier Garcia <javitid@gmail.com>
2014-03-04 21:19:06 +0100

    Proyecto completo v1.1

---
+++ src/com/latarce/location/dondeestamicoche/About.java
@@ -0,0 +1,12 @@
+package com.latarce.location.dondeestamicoche;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class About extends Activity{
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+	}
+}
